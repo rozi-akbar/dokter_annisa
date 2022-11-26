85d8c450c71b6868f32792e7b06644663cb383f1
rozi <rozi@msglowid.com>
2022-11-26 23:59:04 +0700

    add moment js and fix jadwal insert

--- routes/jadwal.js
+++ routes/jadwal.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jadwal = express.Router();
 const koneksi = require('../config/database');
 const auth = require('../middleware/auth');
+const moment = require('moment');
 
 jadwal.post('/', auth, (req, res) => {
 
@@ -9,28 +10,42 @@ jadwal.post('/', auth, (req, res) => {
 
     if (date_range.includes(" s/d ")) {
         const date = date_range.split(" s/d "); //split date
-        const date1 = new Date(date[0]);
-        const date2 = new Date(date[1]);
 
-        for (var i = date1; i <= date2; i.setDate(i.getDate() + 1)) {
-            var d_date = new Date(i).toISOString().slice(0, 10);
+        const dday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-            let querySql = "INSERT INTO t_jadwal SET doctor_id = "+req.body.doctor_id+", day = '"+req.body.day+"', time_start = '"+req.body.time_start+"', time_finish = '"+req.body.time_finish+"', quota = "+req.body.quota+", status = "+req.body.status+", date = '"+d_date+"'";
+        let start = moment(date[0]);
+        let end = moment(date[1]);
+        let day = dday.findIndex(day_index);
+
+        var current = start.clone();
+
+        while (current.day(day).isSameOrBefore(end)) {
+            var d_date = moment(current.clone()).format('YYYY-MM-DD');
+            let querySql = "INSERT INTO t_jadwal SET doctor_id = " + req.body.doctor_id + ", day = '" + req.body.day + "', time_start = '" + req.body.time_start + "', time_finish = '" + req.body.time_finish + "', quota = " + req.body.quota + ", status = " + req.body.status + ", date = '" + d_date + "'";
             koneksi.query(querySql);
+            current.day(7 + day);
         }
+
         res.status(200).json({
             success: true,
-            message: "Data save successfully"    
+            message: "Data save successfully"
+        });
+    } else {
+        res.status(400).json({
+            success: false,
+            message: "No date range"
         });
     }
-    res.status(400).json({
-        success: false,
-        message: "No date range"
-    });
+
+    function day_index(day_name) {
+        if (day_name === req.body.day) {
+            return day_name;
+        }
+    }
 });
 
 jadwal.get('/', auth, (req, res) => {
-    const querySql = 'SELECT id, doctor_id, day, time_start, time_finish, quota, status, date FROM t_jadwal WHERE is_deleted = 0';
+    const querySql = 'SELECT t1.id, t1.doctor_id, t2.nama_dokter, t1.day, t1.time_start, t1.time_finish, t1.quota, t1.status, t1.date FROM t_jadwal t1 JOIN m_dokter t2 ON t1.doctor_id = t2.pid WHERE t1.is_deleted = 0';
 
     // jalankan query
     koneksi.query(querySql, (err, rows, field) => {
@@ -46,7 +61,7 @@ jadwal.get('/', auth, (req, res) => {
 });
 
 jadwal.get('/:id', auth, (req, res) => {
-    const querySql = 'SELECT id, doctor_id, day, time_start, time_finish, quota, status, date FROM t_jadwal where pid = ' + req.params.id;
+    const querySql = 'SELECT id, doctor_id, day, time_start, time_finish, quota, status, date FROM t_jadwal t1 JOIN m_dokter t2 ON t1.doctor_id = t2.pid where pid = ' + req.params.id;
 
     // jalankan query
     koneksi.query(querySql, (err, rows, field) => {
